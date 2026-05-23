4af9287dc0f32fc5fef3051a7295266c359866ff
Zharfan <zharfanwidyadana@gmail.com>
2026-05-24 03:57:51 +0700

    update model kurangi "State"

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 # ✅ LOAD ALL MODELS & SCALERS
 # ============================================
 
-model_full = joblib.load("model_full_39_cnn_lstm.pkl")
+model_full = joblib.load("model_full_38_cnn_lstm.pkl")
 model_mi = joblib.load("model_mi_10_cnn_lstm.pkl")
 model_rf = joblib.load("model_rf_10_cnn_lstm.pkl")
 
